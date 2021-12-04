7f941001fa9dd51fb1ed2387cfd8bc984487c772
David Hotham <david.hotham@metaswitch.com>
2021-12-04 23:20:10 +0000

    f-string

--- hunt/views.py
+++ hunt/views.py
@@ -31,7 +31,7 @@ def get_hunt_events(request: HttpRequest) -> HttpResponse:
     field_names = [field.name for field in meta.fields]
 
     response = HttpResponse(content_type="text/csv")
-    response["Content-Disposition"] = "attachment; filename={}.csv".format(meta)
+    response["Content-Disposition"] = f"attachment; filename={meta}.csv"
     writer = csv.writer(response)
 
     writer.writerow(field_names)
