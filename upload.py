ba471f394d438cc62102690e84cc7923f1660599
David Hotham <david.hotham@blueyonder.co.uk>
2022-12-23 11:44:43 +0000

    updates

--- upload.py
+++ upload.py
@@ -20,15 +20,15 @@ CONTENT_TYPES = {
 }
 
 
-def upload_level_without_hints(level: int, dir: Path) -> None:
+def upload_level_without_hints(level: int, path: Path) -> None:
     """
     Upload a level, without creating any of its hints.
 
     :param level: The level to upload.
-    :param dir: A directory containing about.json and (optionally) blurb.txt.
-                The blurb, if present, is shown on the _next_ level.
+    :param path: A directory containing about.json and (optionally) blurb.txt.
+                 The blurb, if present, is shown on the _next_ level.
     """
-    about = dir / "about.json"
+    about = path / "about.json"
     with open(about, encoding="utf-8") as f:
         data = json.load(f)
 
@@ -36,7 +36,7 @@ def upload_level_without_hints(level: int, dir: Path) -> None:
 
     # It's OK for there to be no blurb, but we report it in case that wasn't what was
     # intended.
-    blurb = dir / "blurb.txt"
+    blurb = path / "blurb.txt"
     try:
         with open(blurb, encoding="utf-8") as f:
             data["description"] = f.read()
@@ -83,30 +83,30 @@ def upload_hint(level: int, hint: int, image: Path) -> None:
     r.raise_for_status()
 
 
-def upload_level(level: int, dir: Path) -> None:
+def upload_level(level: int, path: Path) -> None:
     """
     Upload a level from a directory, creating all of its hints.
 
     :param level: The level to upload.
-    :param dir: A directory containing five images, about.json and (optionally)
-                blurb.txt.
-                The blurb, if present, is shown on the _next_ level.
+    :param path: A directory containing five images, about.json and (optionally)
+                 blurb.txt.
+                 The blurb, if present, is shown on the _next_ level.
     """
     print(f"Uploading level {level}")
 
     # Find the images.
     images = [
-        dir / file
-        for file in os.listdir(dir)
+        path / file
+        for file in os.listdir(path)
         if Path(file).suffix.lower() in CONTENT_TYPES
     ]
 
     # Should find exactly the right number - check the file extensions if not.
     if len(images) != HINTS_PER_LEVEL:
-        raise RuntimeError(f"Found {len(images)} images in {dir}")
+        raise RuntimeError(f"Found {len(images)} images in {path}")
 
     # Create the level.
-    upload_level_without_hints(level, dir)
+    upload_level_without_hints(level, path)
 
     # Upload the hints.
     images.sort(key=lambda x: x.name.lower())
@@ -118,11 +118,11 @@ def upload_level(level: int, dir: Path) -> None:
 
 def main() -> None:
     # Uploads three levels, including the dummy levels 0 and 4.
-    upload_level(level=0, dir=Path("/directory/containing/dummy/level"))
-    upload_level(level=1, dir=Path("/directory/containing/level/one"))
-    upload_level(level=2, dir=Path("/directory/containing/level/two"))
-    upload_level(level=3, dir=Path("/directory/containing/level/three"))
-    upload_level(level=4, dir=Path("/directory/containing/dummy/level"))
+    upload_level(level=0, path=Path("/directory/containing/dummy/level"))
+    upload_level(level=1, path=Path("/directory/containing/level/one"))
+    upload_level(level=2, path=Path("/directory/containing/level/two"))
+    upload_level(level=3, path=Path("/directory/containing/level/three"))
+    upload_level(level=4, path=Path("/directory/containing/dummy/level"))
 
 
 if __name__ == "__main__":
