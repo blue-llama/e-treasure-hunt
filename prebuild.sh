6e298f46c064a88cf848b6d823e73716d5fd818e
David Hotham <david.hotham@blueyonder.co.uk>
2023-01-28 21:10:08 +0000

    miscellany
    - don't download the poetry installer every time
    - unnecessary terraform quoting
    - updates

--- prebuild.sh
+++ prebuild.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 set -euo pipefail
 
-curl -sSL https://install.python-poetry.org | python3
-/home/.local/bin/poetry export -f requirements.txt -o requirements.txt --without-hashes --extras azure
+POETRY="/home/.local/bin/poetry"
+if [[ ! -f ${POETRY} ]]; then
+  curl -sSL https://install.python-poetry.org | python3
+fi
+${POETRY} export -f requirements.txt -o requirements.txt --without-hashes --extras azure
