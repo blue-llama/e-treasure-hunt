785be1a2fdb0fa1ed0d0b14c1508c9163f5b0f3c
David Hotham <david.hotham@blueyonder.co.uk>
2023-01-28 14:35:51 +0000

    update min terraform providers

--- azure/main.tf
+++ azure/main.tf
@@ -3,11 +3,11 @@ terraform {
   required_providers {
     azuread = {
       source  = "hashicorp/azuread"
-      version = "~> 2.30"
+      version = "~> 2.33"
     }
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.32"
+      version = "~> 3.41"
     }
     random = {
       source  = "hashicorp/random"
